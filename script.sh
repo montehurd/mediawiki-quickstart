45531803173eaaaf994d4abb9ff4627c3a14d9e1
montehurd <mhurd@wikimedia.org>
2023-05-26 13:40:38 -0700

    Lint both .sh files consistently

--- script.sh
+++ script.sh
@@ -19,124 +19,124 @@ export MW_ENV
 SPECIAL_VERSION_URL="http://localhost:$MEDIAWIKI_PORT/wiki/Special:Version"
 
 fresh_install() {
-	stop
-	remove
-	prepare
-	start
+  stop
+  remove
+  prepare
+  start
 }
 
 prepare() {
-	mkdir -p "$MEDIAWIKI_DIR"
-	cd "$MEDIAWIKI_DIR"
-	git clone https://gerrit.wikimedia.org/r/mediawiki/core.git . --depth=1
-	echo "$MW_ENV" > .env
-	cp ./docker-compose.override.yml "$MEDIAWIKI_DIR/docker-compose.override.yml"
+  mkdir -p "$MEDIAWIKI_DIR"
+  cd "$MEDIAWIKI_DIR"
+  git clone https://gerrit.wikimedia.org/r/mediawiki/core.git . --depth=1
+  echo "$MW_ENV" >.env
+  cp ./docker-compose.override.yml "$MEDIAWIKI_DIR/docker-compose.override.yml"
 }
 
 remove() {
-	if [ -d "$MEDIAWIKI_DIR" ]; then
-		read -p "Are you sure you want to delete mediawiki containers and EVERYTHING in \"$MEDIAWIKI_DIR\" (y/n)? " -n 1 -r
-		echo
-		if [ "$REPLY" = "y" ]; then
-			cd "$MEDIAWIKI_DIR"
-			docker compose down
-			rm -rf "$MEDIAWIKI_DIR"
-			rm "$SCRIPT_DIR/runonce"
-		fi
-	fi
+  if [ -d "$MEDIAWIKI_DIR" ]; then
+    read -p "Are you sure you want to delete mediawiki containers and EVERYTHING in \"$MEDIAWIKI_DIR\" (y/n)? " -n 1 -r
+    echo
+    if [ "$REPLY" = "y" ]; then
+      cd "$MEDIAWIKI_DIR"
+      docker compose down
+      rm -rf "$MEDIAWIKI_DIR"
+      rm "$SCRIPT_DIR/runonce"
+    fi
+  fi
 }
 
 stop() {
-	cd "$MEDIAWIKI_DIR"
-	docker compose stop
+  cd "$MEDIAWIKI_DIR"
+  docker compose stop
 }
 
 start() {
-	if [ ! -f "$SCRIPT_DIR/runonce" ]; then
-		cd "$MEDIAWIKI_DIR"
-		docker compose up -d
-		docker compose exec mediawiki composer update
-		docker compose exec mediawiki bash /docker/install.sh
-		sleep 2
-		cd "$SCRIPT_DIR"
-		touch runonce
-		use_vector_skin
-		return 0
-	fi
-	is_running=$("$SCRIPT_DIR/utility.sh" is_container_running "mediawiki-mediawiki-1")
-	if [ "$is_running" = false ]; then
-		cd "$MEDIAWIKI_DIR"
-		docker compose up -d
-		sleep 1
-	fi
-	"$SCRIPT_DIR/utility.sh" wait_until_url_available $SPECIAL_VERSION_URL
-	open_special_version_page
+  if [ ! -f "$SCRIPT_DIR/runonce" ]; then
+    cd "$MEDIAWIKI_DIR"
+    docker compose up -d
+    docker compose exec mediawiki composer update
+    docker compose exec mediawiki bash /docker/install.sh
+    sleep 2
+    cd "$SCRIPT_DIR"
+    touch runonce
+    use_vector_skin
+    return 0
+  fi
+  is_running=$("$SCRIPT_DIR/utility.sh" is_container_running "mediawiki-mediawiki-1")
+  if [ "$is_running" = false ]; then
+    cd "$MEDIAWIKI_DIR"
+    docker compose up -d
+    sleep 1
+  fi
+  "$SCRIPT_DIR/utility.sh" wait_until_url_available $SPECIAL_VERSION_URL
+  open_special_version_page
 }
 
 restart() {
-	stop
-	start
+  stop
+  start
 }
 
 bash_mw() {
-	cd "$MEDIAWIKI_DIR"
-	docker compose exec mediawiki bash
+  cd "$MEDIAWIKI_DIR"
+  docker compose exec mediawiki bash
 }
 
 bash_jr() {
-	cd "$MEDIAWIKI_DIR"
-	docker compose exec mediawiki-jobrunner bash
+  cd "$MEDIAWIKI_DIR"
+  docker compose exec mediawiki-jobrunner bash
 }
 
 bash_wb() {
-	cd "$MEDIAWIKI_DIR"
-	docker compose exec mediawiki-web bash
+  cd "$MEDIAWIKI_DIR"
+  docker compose exec mediawiki-web bash
 }
 
 use_vector_skin() {
-	set -k
-	"$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=Vector skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/Vector.git skinBranch=master wfLoadSkin=Vector wgDefaultSkin=vector
-	open_special_version_page
+  set -k
+  "$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=Vector skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/Vector.git skinBranch=master wfLoadSkin=Vector wgDefaultSkin=vector
+  open_special_version_page
 }
 
 use_apiportal_skin() {
-	set -k
-	"$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=WikimediaApiPortal skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/WikimediaApiPortal.git skinBranch=master wfLoadSkin=WikimediaApiPortal wgDefaultSkin=WikimediaApiPortal
-	open_special_version_page
+  set -k
+  "$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=WikimediaApiPortal skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/WikimediaApiPortal.git skinBranch=master wfLoadSkin=WikimediaApiPortal wgDefaultSkin=WikimediaApiPortal
+  open_special_version_page
 }
 
 use_minervaneue_skin() {
-	set -k
-	"$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=MinervaNeue skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/MinervaNeue.git skinBranch=master wfLoadSkin=MinervaNeue wgDefaultSkin=minerva
-	open_special_version_page
+  set -k
+  "$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=MinervaNeue skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/MinervaNeue.git skinBranch=master wfLoadSkin=MinervaNeue wgDefaultSkin=minerva
+  open_special_version_page
 }
 
 use_timeless_skin() {
-	set -k
-	"$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=Timeless skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/Timeless.git skinBranch=master wfLoadSkin=Timeless wgDefaultSkin=timeless
-	open_special_version_page
+  set -k
+  "$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=Timeless skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/Timeless.git skinBranch=master wfLoadSkin=Timeless wgDefaultSkin=timeless
+  open_special_version_page
 }
 
 use_monobook_skin() {
-	set -k
-	"$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=MonoBook skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/MonoBook.git skinBranch=master wfLoadSkin=MonoBook wgDefaultSkin=monobook
-	open_special_version_page
+  set -k
+  "$SCRIPT_DIR/utility.sh" apply_mediawiki_skin mediawikiPath=$MEDIAWIKI_DIR skinSubdirectory=MonoBook skinRepoURL=https://gerrit.wikimedia.org/r/mediawiki/skins/MonoBook.git skinBranch=master wfLoadSkin=MonoBook wgDefaultSkin=monobook
+  open_special_version_page
 }
 
 open_special_version_page() {
-	if [ "$skipopenspecialversionpage" != "true" ]; then
-		"$SCRIPT_DIR/utility.sh" open_url_when_available $SPECIAL_VERSION_URL
-	fi
+  if [ "$skipopenspecialversionpage" != "true" ]; then
+    "$SCRIPT_DIR/utility.sh" open_url_when_available $SPECIAL_VERSION_URL
+  fi
 }
 
 run_parser_tests() {
-	cd "$MEDIAWIKI_DIR"
-	docker compose exec mediawiki php tests/parser/parserTests.php
+  cd "$MEDIAWIKI_DIR"
+  docker compose exec mediawiki php tests/parser/parserTests.php
 }
 
 run_php_unit_tests() {
-	cd "$MEDIAWIKI_DIR"
-	docker compose exec --workdir /var/www/html/w/tests/phpunit mediawiki php phpunit.php ${testpath:+$testpath} ${testgroup:+--group $testgroup} --testdox
+  cd "$MEDIAWIKI_DIR"
+  docker compose exec --workdir /var/www/html/w/tests/phpunit mediawiki php phpunit.php ${testpath:+$testpath} ${testgroup:+--group $testgroup} --testdox
 }
 
 "$@"
--- utility.sh
+++ utility.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
 get_response_code() {
-    echo $(curl --write-out '%{http_code}' --silent --output /dev/null "$1");
+  echo $(curl --write-out '%{http_code}' --silent --output /dev/null "$1")
 }
 
 is_container_running() {
-    is_running=$(docker inspect -f '{{.State.Running}}' "$1" 2>/dev/null);
-    echo "${is_running:=false}";
+  is_running=$(docker inspect -f '{{.State.Running}}' "$1" 2>/dev/null)
+  echo "${is_running:=false}"
 }
 
 open_url_when_available() {
-    wait_until_url_available "$1";
-    error_message="Unable to automatically open '$1', try opening it in a browser"
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        # macOS system
-        open ${2:+-a "$2"} "$1" || echo "$error_message"
-    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-        # Linux system
-        xdg-open "$1" || echo "$error_message"
-    elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
-        # Windows system (Cygwin, Git Bash, or WSL)
-        start "" "$1" || echo "$error_message"
-    else
-        echo "Unsupported operating system"
-    fi
+  wait_until_url_available "$1"
+  error_message="Unable to automatically open '$1', try opening it in a browser"
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS system
+    open ${2:+-a "$2"} "$1" || echo "$error_message"
+  elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Linux system
+    xdg-open "$1" || echo "$error_message"
+  elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    # Windows system (Cygwin, Git Bash, or WSL)
+    start "" "$1" || echo "$error_message"
+  else
+    echo "Unsupported operating system"
+  fi
 }
 
 wait_until_url_available() {
-    while ! [[ "$(get_response_code $1)" =~ ^(200|301)$ ]]; do sleep 1; done;
-    sleep 0.5;
+  while ! [[ "$(get_response_code $1)" =~ ^(200|301)$ ]]; do sleep 1; done
+  sleep 0.5
 }
 
 apply_mediawiki_skin_settings() {
-    cd "$mediawikiPath";
-    grep -qx "^wfLoadSkin([\"']$wfLoadSkin[\"']); *$" LocalSettings.php || echo "wfLoadSkin(\"$wfLoadSkin\");" >> LocalSettings.php;
-    sed -i -E "s/\\\$wgDefaultSkin.*;[[:blank:]]*$/\\\$wgDefaultSkin = \"$wgDefaultSkin\";/g" LocalSettings.php;
+  cd "$mediawikiPath"
+  grep -qx "^wfLoadSkin([\"']$wfLoadSkin[\"']); *$" LocalSettings.php || echo "wfLoadSkin(\"$wfLoadSkin\");" >>LocalSettings.php
+  sed -i -E "s/\\\$wgDefaultSkin.*;[[:blank:]]*$/\\\$wgDefaultSkin = \"$wgDefaultSkin\";/g" LocalSettings.php
 }
 
 apply_mediawiki_skin() {
-    cd "$mediawikiPath";
-    rm -rf "skins/$skinSubdirectory";
-    git clone --branch "$skinBranch" "$skinRepoURL" "./skins/$skinSubdirectory" --depth=1;
-    sleep 1;
-    apply_mediawiki_skin_settings;
+  cd "$mediawikiPath"
+  rm -rf "skins/$skinSubdirectory"
+  git clone --branch "$skinBranch" "$skinRepoURL" "./skins/$skinSubdirectory" --depth=1
+  sleep 1
+  apply_mediawiki_skin_settings
 }
 
 apply_mediawiki_extension_settings() {
-    cd "$mediawikiPath";
-    grep -qx "^[[:blank:]]*wfLoadExtension[[:blank:]]*([[:blank:]]*[\"']$wfLoadExtension[\"'][[:blank:]]*)[[:blank:]]*;[[:blank:]]*$" LocalSettings.php || echo "wfLoadExtension(\"$wfLoadExtension\");" >> LocalSettings.php;
+  cd "$mediawikiPath"
+  grep -qx "^[[:blank:]]*wfLoadExtension[[:blank:]]*([[:blank:]]*[\"']$wfLoadExtension[\"'][[:blank:]]*)[[:blank:]]*;[[:blank:]]*$" LocalSettings.php || echo "wfLoadExtension(\"$wfLoadExtension\");" >>LocalSettings.php
 }
 
 apply_mediawiki_extension() {
-    cd "$mediawikiPath";
-    rm -rf "extensions/$extensionSubdirectory";
-    git clone --branch "$extensionBranch" "$extensionRepoURL" "./extensions/$extensionSubdirectory" --depth=1;
-    sleep 1;
-    apply_mediawiki_extension_settings;
+  cd "$mediawikiPath"
+  rm -rf "extensions/$extensionSubdirectory"
+  git clone --branch "$extensionBranch" "$extensionRepoURL" "./extensions/$extensionSubdirectory" --depth=1
+  sleep 1
+  apply_mediawiki_extension_settings
 }
 
 "$@"
