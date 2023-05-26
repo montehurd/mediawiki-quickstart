8b4ac16842888b830de2eb32a770172ae8ce7134
montehurd <mhurd@wikimedia.org>
2023-05-26 11:30:17 -0700

    Add missing quotes

--- utility.sh
+++ utility.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 get_response_code() {
-    echo $(curl --write-out '%{http_code}' --silent --output /dev/null $1);
+    echo $(curl --write-out '%{http_code}' --silent --output /dev/null "$1");
 }
 
 is_container_running() {
-    is_running=$(docker inspect -f '{{.State.Running}}' $1 2>/dev/null);
+    is_running=$(docker inspect -f '{{.State.Running}}' "$1" 2>/dev/null);
     echo "${is_running:=false}";
 }
 
