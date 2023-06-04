58972a0f664144b18ec464b083d5430cfc3a6e8b
montehurd <mhurd@wikimedia.org>
2023-06-03 18:33:48 -0700

    Make selenium prep funcs callable more than once

--- selenium-preparation.sh
+++ selenium-preparation.sh
@@ -3,17 +3,23 @@
 apply_patch() {
   echo "apply patch"
   curl "https://gerrit.wikimedia.org/r/changes/915838/revisions/2/patch?download" | base64 --decode >patch.diff
-  patch -p1 <patch.diff
+  if patch --dry-run -p1 -N <patch.diff; then
+    patch -p1 -N <patch.diff
+  else
+    echo "Patch has already been applied or cannot be applied."
+  fi
 }
 
 prepare_node() {
   echo "prepare node"
-  curl -sL https://deb.nodesource.com/setup_16.x | bash -
-  apt-get update
-  apt-get install -y nodejs
-  npm install puppeteer-chromium-version-finder
-  npm ci
-  npm audit fix --force
+  if ! command -v node > /dev/null; then
+    curl -sL https://deb.nodesource.com/setup_16.x | bash -
+    apt-get update
+    apt-get install -y nodejs
+    npm install puppeteer-chromium-version-finder
+    npm ci
+    npm audit fix --force
+  fi
 }
 
 # prepare_chromium () {
