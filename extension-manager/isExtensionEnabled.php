e45a5782d9875d5e038284f6ba3527d8980604bf
montehurd <mhurd@wikimedia.org>
2023-06-16 15:22:41 -0700

    First pass at an extension installer

---
+++ extension-manager/isExtensionEnabled.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/Maintenance.php';
+
+class isExtensionEnabled extends Maintenance {
+    public function __construct() {
+        parent::__construct();
+        $this->addOption( 'extension', 'The extension to check', true, true );
+    }
+
+    public function execute() {
+        $extension = $this->getOption( 'extension' );
+        $normalizedExtension = $this->normalizeExtensionName($extension);
+        $loadedExtensions = $this->getAllNormalizedLoadedExtensions();
+        if (in_array($normalizedExtension, $loadedExtensions)) {
+            $this->output("1");
+        } else {
+            $this->output("0");
+        }
+    }
+
+    private function normalizeExtensionName($extension) {
+        return strtolower(str_replace(' ', '', $extension));
+    }
+
+    private function getAllNormalizedLoadedExtensions() {
+        $registry = ExtensionRegistry::getInstance();
+        $loadedExtensions = array_keys($registry->getAllThings());
+        return array_map([$this, 'normalizeExtensionName'], $loadedExtensions);
+    }
+}
+
+$maintClass = isExtensionEnabled::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ extension-manager/script.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -eu
+
+docker cp "$(pwd)/isExtensionEnabled.php" mediawiki-mediawiki-1:/var/www/html/w/maintenance/isExtensionEnabled.php
+
+required_keys=('name' 'repository' 'configuration' 'bashScripts')
+MEDIAWIKI_PATH="../mediawiki"
+
+yq() {
+    echo "$2" | docker run --rm -i -v "${PWD}:/workdir" mikefarah/yq eval "$1" -
+}
+
+# Function to validate if all required keys are present in the manifest
+validate_keys() {
+    local manifest_content=$1
+    for key in "${required_keys[@]}"; do
+        value=$(yq ".${key}" "$manifest_content")
+        # echo "Key $key has value: $value"
+        if [ -z "$value" ] || [ "$value" = "null" ]; then
+            echo "Missing key $key in manifest, skipping..."
+            return 1
+        fi
+    done
+    return 0
+}
+
+extension_is_enabled() {
+    local extension_name=$1
+    local output=$(docker exec -it mediawiki-mediawiki-1 php maintenance/run.php isExtensionEnabled --extension="$extension_name")
+    if [ "$output" == "1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Function to process the manifest and install the extension
+process_manifest() {
+    local manifest_content=$1
+    name=$(yq '.name' "$manifest_content")
+    repository=$(yq '.repository' "$manifest_content")
+    configuration=$(yq '.configuration' "$manifest_content")
+    bashScripts=$(yq '.bashScripts' "$manifest_content")
+
+    # Check if the extension is already installed
+    if extension_is_enabled "$name"; then
+        echo "Extension $name is already installed and active, skipping..."
+        return
+    fi
+
+    # Clone the extension
+    if ! git clone "$repository" "$MEDIAWIKI_PATH/extensions/$name" --depth=1 2>&1; then
+        echo "Failed to clone repository $repository"
+        exit 1
+    fi
+
+    # Append the configuration to LocalSettings.php with a comment
+    echo -e "\n# Configuration for $name extension" >> "$MEDIAWIKI_PATH/LocalSettings.php"
+    echo -e "$configuration" >> "$MEDIAWIKI_PATH/LocalSettings.php"
+
+    # Check if the MediaWiki container is running
+    if [ "$(docker inspect -f '{{.State.Running}}' mediawiki-mediawiki-1)" = "true" ]; then
+        # Run the bash scripts inside the MediaWiki container
+        docker exec mediawiki-mediawiki-1 bash -c "$bashScripts"
+    else
+        echo "MediaWiki container is not running"
+    fi
+}
+
+process_all_manifests() {
+    for manifest in ./manifests/*.yaml
+    do
+        # Parse the manifest file once
+        manifest_content=$(cat "$manifest")
+
+        # Validate the keys and process the manifest if valid
+        if validate_keys "$manifest_content"; then
+            process_manifest "$manifest_content"
+        else
+            echo "Invalid manifest $manifest, skipping..."
+        fi
+    done
+}
+
+process_all_manifests
