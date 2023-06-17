5a3003d798327a77f83415084fc4552083ef09eb
montehurd <mhurd@wikimedia.org>
2023-06-16 18:32:40 -0700

    Remove unneeded lines

--- extension-manager/script.sh
+++ extension-manager/script.sh
@@ -16,13 +16,11 @@ yq() {
   echo "$2" | docker run --rm -i -v "${PWD}:/workdir" mikefarah/yq eval "$1" -
 }
 
-# Function to validate if all required keys are present in the manifest
 validate_keys() {
   local manifest_content
   manifest_content=$1
   for key in "${required_keys[@]}"; do
     value=$(yq ".${key}" "$manifest_content")
-    # echo "Key $key has value: $value"
     if [ -z "$value" ] || [ "$value" = "null" ]; then
       echo "Missing key $key in manifest, skipping..."
       return 1
@@ -43,7 +41,6 @@ extension_is_enabled() {
   fi
 }
 
-# Function to process the manifest and install the extension
 process_manifest() {
   local manifest_content
   manifest_content=$1
@@ -51,33 +48,22 @@ process_manifest() {
   repository=$(yq '.repository' "$manifest_content")
   configuration=$(yq '.configuration' "$manifest_content")
   bashScripts=$(yq '.bashScripts' "$manifest_content")
-
-  # Check if the extension is already installed
   if extension_is_enabled "$name"; then
     echo "Extension $name is already installed and active, skipping..."
     return
   fi
-
-  # Clone the extension
   if ! git clone "$repository" "$MEDIAWIKI_PATH/extensions/$name" --depth=1 2>&1; then
     echo "Failed to clone repository $repository"
     exit 1
   fi
-
-  # Append the configuration to LocalSettings.php with a comment
   echo -e "\n# Configuration for $name extension" >>"$MEDIAWIKI_PATH/LocalSettings.php"
   echo -e "$configuration" >>"$MEDIAWIKI_PATH/LocalSettings.php"
-
-  # Run the bash scripts inside the MediaWiki container
   docker exec mediawiki-mediawiki-1 bash -c "$bashScripts"
 }
 
 process_all_manifests() {
   for manifest in ./manifests/*.yaml; do
-    # Parse the manifest file once
     manifest_content=$(cat "$manifest")
-
-    # Validate the keys and process the manifest if valid
     if validate_keys "$manifest_content"; then
       process_manifest "$manifest_content"
     else
