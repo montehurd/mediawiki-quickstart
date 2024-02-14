d05ec8f31117833c59ce31c45c209c3992a7b886
montehurd <mhurd@wikimedia.org>
2024-02-13 16:04:49 -0800

    return indication submodule was updated or not

--- common/submodule_utility.sh
+++ common/submodule_utility.sh
@@ -2,11 +2,23 @@
 
 ensure_submodule_initialized_and_updated() {
   local submodule
+  local before_update_sha
+  local after_update_sha
   submodule="$1"
   if [ ! -e "./$submodule/.git" ]; then
     git submodule update --init --recursive --remote "$submodule"
+    echo "Submodule '$submodule' was updated (initial fetch)"
+    return 0
   else
+    before_update_sha=$(git -C "./$submodule" rev-parse HEAD)
     git submodule update --recursive "$submodule"
+    after_update_sha=$(git -C "./$submodule" rev-parse HEAD)
+    if [ "$before_update_sha" != "$after_update_sha" ]; then
+      echo "Submodule '$submodule' was updated"
+      return 0
+    fi
+    echo "Submodule '$submodule' is already up to date"
+    return 1
   fi
 }
 
