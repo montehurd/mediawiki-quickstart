600c679cae515d2e682b41a20aef06fe17fa9a77
montehurd <mhurd@wikimedia.org>
2024-08-21 13:12:19 -0700

    New approach no longer uses this npm package

--- selenium/puppeteer-chromium-version-finder.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const versionFinder = require( 'puppeteer-chromium-version-finder' );
-
-( async () => {
-	const versionObj = await versionFinder.getPuppeteerChromiumVersion();
-	const versionStr = `${versionObj.MAJOR}.${versionObj.MINOR}.${versionObj.BUILD}.${versionObj.PATCH}`;
-	console.log( versionStr );
-} )();
