910c74266a37c60305aec981bdcff436b680db36
montehurd <mhurd@wikimedia.org>
2024-08-23 10:31:24 -0700

    Make extension node dependency installer do 'npm ci' if package-lock.json present

--- extensions/node.sh
+++ extensions/node.sh
@@ -6,11 +6,13 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 MEDIAWIKI_PATH="$SCRIPT_PATH/../mediawiki"
 EXTENSIONS_PATH="$MEDIAWIKI_PATH/extensions"
 
-_extensions_with_package_json() {
+_extensions_with_file() {
+    local file_name="$1"
+    shift
     local extensions=("$@")
     local valid_extensions=()
     for extension in "${extensions[@]}"; do
-        if [[ -f "$EXTENSIONS_PATH/$extension/package.json" ]]; then
+        if [[ -f "$EXTENSIONS_PATH/$extension/$file_name" ]]; then
             valid_extensions+=("$extension")
         fi
     done
@@ -18,17 +20,38 @@ _extensions_with_package_json() {
 }
 
 install_node_dependencies_for_extensions() {
-    local extensions=($(_extensions_with_package_json "$@"))
-    if [[ ${#extensions[@]} -eq 0 ]]; then
-        echo "No extensions with package.json found. Exiting."
+    local extensions=("$@")
+
+    local extensions_with_package_lock_json=($(_extensions_with_file "package-lock.json" "${extensions[@]}"))
+    local extensions_with_package_json=($(_extensions_with_file "package.json" "${extensions[@]}"))
+    
+    if [[ ${#extensions_with_package_lock_json[@]} -eq 0 ]] && [[ ${#extensions_with_package_json[@]} -eq 0 ]]; then
+        echo "No extensions with package-lock.json or package.json found. Exiting."
         return 0
     fi
-    echo -e "\nInstalling Node dependencies for extensions: ${extensions[@]}"
-    docker exec -it -u root mediawiki-mediawiki-web-1 bash -c '
-        SECONDS=0
-        printf "./extensions/%s\\n" "$@" | xargs -I {} -P 2 sh -c "cd {} && echo \"Installing Node dependencies for \$(pwd)\" && npm install > ./npm_install.log 2>&1"
-        ELAPSED=$SECONDS
-        echo "Duration: "$((ELAPSED/60))" minutes "$((ELAPSED%60))" seconds"
-    ' _ "${extensions[@]}"
-    return $?
+    
+    SECONDS=0
+
+    if [[ ${#extensions_with_package_lock_json[@]} -gt 0 ]]; then
+        echo -e "\nInstalling Node dependencies for extensions with package-lock.json: ${extensions_with_package_lock_json[@]}"
+        docker exec -it -u root mediawiki-mediawiki-web-1 bash -c '
+            printf "./extensions/%s\n" "$@" | xargs -I {} sh -c "cd {} && echo \"Installing Node dependencies for \$(pwd) using npm ci\" && npm ci > ./npm_ci.log 2>&1"
+        ' _ "${extensions_with_package_lock_json[@]}"
+    fi
+    
+    # Remove extensions with package-lock.json from extensions_with_package_json
+    # ie only do a "npm install" if we haven't already done a "npm ci"
+    for ext in "${extensions_with_package_lock_json[@]}"; do
+        extensions_with_package_json=(${extensions_with_package_json[@]/$ext})
+    done
+
+    if [[ ${#extensions_with_package_json[@]} -gt 0 ]]; then
+        echo -e "Installing Node dependencies for extensions with only package.json: ${extensions_with_package_json[@]}"
+        docker exec -it -u root mediawiki-mediawiki-web-1 bash -c '
+            printf "./extensions/%s\n" "$@" | xargs -I {} sh -c "cd {} && echo \"Installing Node dependencies for \$(pwd) using npm install\" && npm install > ./npm_install.log 2>&1"
+        ' _ "${extensions_with_package_json[@]}"
+    fi
+
+    ELAPSED=$SECONDS
+    echo "Duration for node dependency installation: $((ELAPSED/60)) minutes $((ELAPSED%60)) seconds"
 }
