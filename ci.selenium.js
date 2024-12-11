c2d8bf7e7489ad09cfbbe4fca5b21e57d054e9b4
montehurd <mhurd@wikimedia.org>
2024-12-11 13:43:10 +0100

    Revert "remove some hardcoded ports"
    This reverts commit fa41432e6d30e7b220f0860631fe1105355d9707.

--- ci.selenium.js
+++ ci.selenium.js
@@ -3,13 +3,13 @@
 
 describe('Quickstart Selenium', () => {
     it('should show Version heading', async () => {
-        await browser.url(`http://localhost:${process.env.MW_DOCKER_PORT}/wiki/Special:Version`);
+        await browser.url('http://localhost:8080/wiki/Special:Version');
         const heading = await $('#firstHeading');
         await expect(heading).toHaveText('Version');
     });
 
     it('should log in', async () => {
-        await browser.url(`http://localhost:${process.env.MW_DOCKER_PORT}/w/index.php?title=Special:UserLogin`);
+        await browser.url('http://localhost:8080/w/index.php?title=Special:UserLogin');
         const usernameInput = await $('#wpName1');
         const passwordInput = await $('#wpPassword1');
         await usernameInput.setValue('Admin');
@@ -20,14 +20,14 @@ describe('Quickstart Selenium', () => {
     });
 
     it('appearance settings should have selected vector radio button', async () => {
-        await browser.url(`http://localhost:${process.env.MW_DOCKER_PORT}/wiki/Special:Preferences#mw-prefsection-rendering`);
+        await browser.url('http://localhost:8080/wiki/Special:Preferences#mw-prefsection-rendering');
         const radioBtn = await $('input[type="radio"][value="vector-2022"]');
         await expect(radioBtn).toBeExisting();
         await expect(radioBtn).toBeSelected();
     });
 
     it('appearance settings should have selected monobook radio button', async () => {
-        await browser.url(`http://localhost:${process.env.MW_DOCKER_PORT}/wiki/Special:Preferences#mw-prefsection-rendering`);
+        await browser.url('http://localhost:8080/wiki/Special:Preferences#mw-prefsection-rendering');
         const radioBtn = await $('input[type="radio"][value="monobook"]');
         await expect(radioBtn).toBeExisting();
         await expect(radioBtn).toBeSelected();
