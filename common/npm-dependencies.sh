345e52e72dbad9e20aac0f26de8dc649176aa641
montehurd <mhurd@wikimedia.org>
2024-12-20 15:54:46 -0600

    use install_node_dependencies function

--- common/npm-dependencies.sh
+++ common/npm-dependencies.sh
@@ -10,16 +10,7 @@ ensure_npm_ready() {
     echo "Mediawiki containers are not running..."
     fresh_install
   fi
-  # TODO: consider moving the bash code below to a "sh" file that the docker exec runs in the container
   docker exec -i \
     -u $(id -u):$(id -g) \
-    -e DISPLAY=mediawiki-novnc-1:0 \
-    mediawiki-mediawiki-web-1 bash <<'EOF'
-    set -e
-    if [ ! -d "node_modules" ]; then
-      npm ci
-    else
-      npm install
-    fi
-EOF
+    mediawiki-mediawiki-web-1 bash /var/local/node-preparation.sh install_node_dependencies
 }
