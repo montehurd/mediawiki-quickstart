4d6873ea3b029d377ab0d14b42df1420e46f1a9f
montehurd <mhurd@wikimedia.org>
2024-12-20 16:50:20 -0600

    remove comment

--- common/container-readiness.sh
+++ common/container-readiness.sh
@@ -7,7 +7,6 @@ fresh_install_if_containers_not_running() {
   if are_containers_running "mediawiki-mediawiki-1" "mediawiki-mediawiki-web-1" "mediawiki-mediawiki-jobrunner-1" "mediawiki-novnc-1"; then
     return
   fi
-  # TODO: output a better message here and maybe the option to start instead of fresh install 
   echo
   echo "Mediawiki containers are not running..."
   fresh_install
