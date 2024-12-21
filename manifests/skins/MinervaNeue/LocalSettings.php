49456bf9966fcf5d8f90405c4aa5df06363e91e9
montehurd <mhurd@wikimedia.org>
2024-12-21 14:08:02 -0600

    fix for skin hacks

--- manifests/skins/MinervaNeue/LocalSettings.php
+++ manifests/skins/MinervaNeue/LocalSettings.php
@@ -1,11 +1,3 @@
 <?php
 
 wfLoadSkin( 'MinervaNeue' );
-
-# HAX: for some reason MinervaNeue can't by loaded by using its name.
-# QuickStart's installer use skins' names. This hack just overrides the
-# actual name with "Minerva" without requiring the installer to have
-# knowledge of this bug
-if (isset($wgDefaultSkin) && $wgDefaultSkin === 'MinervaNeue') {
-    $wgDefaultSkin = 'Minerva';
-}
