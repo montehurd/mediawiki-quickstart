230f939943d37fef917a0005d507e70fe2bd4283
montehurd <mhurd@wikimedia.org>
2024-12-31 21:00:01 -0600

    use run.php in CirrusSearch setup

--- extensions/CirrusSearch/setup.sh
+++ extensions/CirrusSearch/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-php extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php
-php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks --indexOnSkip
-php extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipParse
-php maintenance/runJobs.php
-php extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php
+php maintenance/run.php ./extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php
+php maintenance/run.php ./extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipLinks --indexOnSkip
+php maintenance/run.php ./extensions/CirrusSearch/maintenance/ForceSearchIndex.php --skipParse
+php maintenance/run.php ./maintenance/runJobs.php
+php maintenance/run.php ./extensions/CirrusSearch/maintenance/UpdateSuggesterIndex.php
