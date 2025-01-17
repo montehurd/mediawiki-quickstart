d67f983e174b7995434ace183ffbfa068bc01e3c
montehurd <mhurd@wikimedia.org>
2025-01-16 20:57:30 -0600

    move error handling into method

--- selenium/selenium.sh
+++ selenium/selenium.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 source "./common/container-readiness.sh"
+source "./common/utility.sh"
 
 ensure_selenium_ready() {
+  echo | verboseOrDotPerLine "Ensuring Selenium ready..."
   fresh_install_if_containers_not_running
-  ./shellto -e DISPLAY=mediawiki-novnc-1:0 w /var/local/install-browser-for-puppeteer-core.sh
+  if ! ./shellto -e DISPLAY=mediawiki-novnc-1:0 w /var/local/install-browser-for-puppeteer-core.sh; then
+    echo
+    exit 1
+  fi
 }
