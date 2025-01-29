2b7a23e59a8fb932e2f342e48b7fe875859edf73
Vaughn Walters <vwalters@wikimedia.org>
2025-01-29 19:55:59 +0000

    Wikimedia campaign events
    * removing unnecessary line from LocalSettings.php
    
    * Adding the WikimediaCampaignEvents extension to mediawikiquickstart

---
+++ extensions/WikimediaCampaignEvents/LocalSettings.php
@@ -0,0 +1,8 @@
+<?php
+
+wfLoadExtension( 'WikimediaCampaignEvents' );
+
+$wgWikimediaCampaignEventsFluxxOauthUrl = 'https://wmf.fluxx.io/oauth/token';
+$wgWikimediaCampaignEventsFluxxClientID = 'INSERT_CLIENT_ID_HERE';
+$wgWikimediaCampaignEventsFluxxClientSecret = 'INSERT_CLIENT_SECRET_HERE';
+
