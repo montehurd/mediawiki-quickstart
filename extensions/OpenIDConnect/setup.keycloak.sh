793c0893a15a4bbc033deb8d070140be0a87cf3b
Cicalese <33-cicalese@users.noreply.gitlab.wikimedia.org>
2025-02-03 18:37:12 +0000

    Add support for testing backchannel logout
    * Configure backchannel logout programmatically for OpenID Connect
    
    * Allow disabling backchannel logout tests
    
    Can't enable backchannel logout programmatically yet, so it needs to be
    disabled by dfault.
    
    * Cleaned up keycloak setup script

--- extensions/OpenIDConnect/setup.keycloak.sh
+++ extensions/OpenIDConnect/setup.keycloak.sh
@@ -13,18 +13,23 @@ sleep 15
 # create realm
 /opt/keycloak/bin/kcadm.sh create realms --set enabled=true --set realm=$REALM
 
-# create client, saving client id
-CID=$(/opt/keycloak/bin/kcadm.sh create clients --target-realm $REALM --set clientId=$CLIENT --set 'redirectUris=["http://*"]' --id)
-
-# create client secret for client
-/opt/keycloak/bin/kcadm.sh create clients/${CID}/client-secret --target-realm $REALM
-
 # create user
-/opt/keycloak/bin/kcadm.sh create users --target-realm $REALM --set username=$USERNAME --set email=test@example.com --set firstName=Test --set lastName=User --set enabled=true
+TEST_USER_UUID=$(/opt/keycloak/bin/kcadm.sh create users --target-realm $REALM --set username=$USERNAME --set email=test@example.com --set firstName=Test --set lastName=User --set enabled=true --id)
+echo "TEST_USER_UUID=$TEST_USER_UUID" >> /root/.env
 
 # create password for user
 /opt/keycloak/bin/kcadm.sh set-password --target-realm $REALM --username $USERNAME --new-password $PASSWORD
 
+# create client, saving client id
+CLIENT_UUID=$(/opt/keycloak/bin/kcadm.sh create clients --target-realm $REALM --set clientId=$CLIENT --id)
+echo "CLIENT_UUID=$CLIENT_UUID" >> /root/.env
+
+/opt/keycloak/bin/kcadm.sh update clients/$CLIENT_UUID --target-realm $REALM --set 'redirectUris=["http://*"]'
+/opt/keycloak/bin/kcadm.sh update clients/$CLIENT_UUID --target-realm $REALM --body "{\"attributes\": {\"backchannel.logout.url\": \"http://${HOST_IP}:8080/w/rest.php/pluggableauth/v1/logout\", \"backchannel.logout.session.required\": \"false\"}}"
+
+# create client secret for client
+/opt/keycloak/bin/kcadm.sh create clients/${CLIENT_UUID}/client-secret --target-realm $REALM
+
 # echo client secret
-CLIENT_SECRET=$(/opt/keycloak/bin/kcadm.sh get clients/${CID}/client-secret --target-realm $REALM --fields value --format csv --noquotes)
+CLIENT_SECRET=$(/opt/keycloak/bin/kcadm.sh get clients/${CLIENT_UUID}/client-secret --target-realm $REALM --fields value --format csv --noquotes)
 echo "CLIENT_SECRET=$CLIENT_SECRET" >> /root/.env
