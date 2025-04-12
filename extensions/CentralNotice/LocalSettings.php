31a9a394d467b2b9af2012fcadf52bb7d0f2c1a0
montehurd <mhurd@wikimedia.org>
2025-04-12 17:13:50 -0600

    fix central notice default settings

--- extensions/CentralNotice/LocalSettings.php
+++ extensions/CentralNotice/LocalSettings.php
@@ -1,3 +1,11 @@
 <?php
 
 wfLoadExtension( 'CentralNotice' );
+
+$wgNoticeInfrastructure = true;
+$wgNoticeProject = 'centralnoticeproject'; # 'centralnoticeproject' can be any string
+$wgNoticeProjects = [ $wgNoticeProject ];
+$wgCentralHost = $wgServer;
+$wgCentralSelectedBannerDispatcher = "$wgServer$wgScriptPath/index.php?title=Special:BannerLoader";
+$wgCentralDBname = $wgDBname;
+$wgCentralNoticeGeoIPBackgroundLookupModule = 'ext.centralNotice.freegeoipLookup';
