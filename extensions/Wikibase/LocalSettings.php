237bea7ed3cc75d883f199abdfaddafe171f21bf
montehurd <mhurd@wikimedia.org>
2025-05-01 16:07:21 -0600

    fix wikibase localsettings

--- extensions/Wikibase/LocalSettings.php
+++ extensions/Wikibase/LocalSettings.php
@@ -2,3 +2,6 @@
 
 wfLoadExtension( 'WikibaseRepository', "$IP/extensions/Wikibase/extension-repo.json" );
 require_once "$IP/extensions/Wikibase/repo/ExampleSettings.php";
+
+wfLoadExtension( 'WikibaseClient', "$IP/extensions/Wikibase/extension-client.json" );
+require_once "$IP/extensions/Wikibase/client/ExampleSettings.php";
