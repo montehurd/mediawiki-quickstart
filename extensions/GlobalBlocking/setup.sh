c4f9feefcebcf641b0b3addd743e3602fbce5920
montehurd <mhurd@wikimedia.org>
2025-05-01 14:53:36 -0600

    remove no longer needed backport fix

--- extensions/GlobalBlocking/setup.sh
+++ extensions/GlobalBlocking/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sed -i '/buster-backports/d' /etc/apt/sources.list
-rm -f /etc/apt/sources.list.d/php.list /etc/apt/trusted.gpg.d/php.gpg
 apt update
 apt install -y sqlite3
 sqlite3 cache/sqlite/globalblocking.sqlite < extensions/GlobalBlocking/sql/sqlite/tables-generated-globalblocks.sql
