31e504ed94bdbda1b86c3ebc1925c3a330c13b44
Peter Wangai <pwangai@wikimedia.org>
2025-12-03 20:17:54 -0600

    Segment run logs by project

--- ci.components/vue/src/components/ResultsFooter.vue
+++ ci.components/vue/src/components/ResultsFooter.vue
@@ -3,18 +3,17 @@
     <div v-if="files.length > 0">
       Artifacts
       <span
-        v-for="( file, index ) in files"
-        :key="file"
+          v-for="( file, index ) in files"
+          :key="file"
       >
         <a
-          :href="`/api/results/${file}`"
-          class="footer-link"
+            :href="`/api/results/${file}`"
+            class="footer-link"
         >{{ getExtension( file ) }}</a>
       </span>
       <div class="footer-note">
         <ul>
-          <li>"log.ansi" - cumulative console output of the run, click to download then use "cat" in your terminal to view</li>
-          <li>"yaml" - data file containing cumulative results of run</li>
+          <li>"run.yaml" - data file containing cumulative results of run</li>
         </ul>
       </div>
     </div>
@@ -30,8 +29,8 @@ const props = defineProps( {
 } )
 
 const getExtension = ( filename ) => filename
-  .replace( /^.*[\\/]/, '' )
-  .replace( /^\d+\./, '' )
+    .replace( /^.*[\\/]/, '' )
+    .replace( /^\d+\./, '' )
 </script>
 
 <style scoped>
