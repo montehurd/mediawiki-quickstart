91720c14d408f5d114201ff4af7113eabf1af86d
Mhurd <mhurd@wikimedia.org>
2026-02-23 07:01:24 -0800

    fix page triage selenium config
    * fix page triage selenium config

--- extensions/PageTriage/LocalSettings.php
+++ extensions/PageTriage/LocalSettings.php
@@ -1,3 +1,18 @@
 <?php
 
 wfLoadExtension( 'PageTriage' );
+
+$wgJobRunRate = 100;
+
+// ORES & PageTriage configuration
+$wgPageTriageDraftNamespaceId = 118;
+$wgExtraNamespaces[ $wgPageTriageDraftNamespaceId ] = 'Draft';
+$wgExtraNamespaces[ $wgPageTriageDraftNamespaceId + 1 ] = 'Draft_talk';
+$wgPageTriageNoIndexUnreviewedNewArticles = true;
+$wgPageTriageEnableCopyvio = true;
+$wgPageTriageEnableOresFilters = true;
+$wgOresWikiId = 'enwiki';
+$wgOresModels = [
+	'articlequality' => [ 'enabled' => true, 'namespaces' => [ 0 ], 'cleanParent' => true ],
+	'draftquality' => [ 'enabled' => true, 'namespaces' => [ 0 ], 'types' => [ 1 ] ]
+];
